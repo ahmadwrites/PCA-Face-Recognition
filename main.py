1ee7eace7222f55d91230ebf259780a1903e5625
worknovel <ahmadhasnol@hotmail.com>
2022-02-17 18:51:30 +0800

    Added new PCA implementation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def readImages(path):
     print("Reading images from " + path, end = "...")
     # Create array of array of images.
     images = []
+    classNames = []
     # List all files in the directory and read points from text files one by one.
     for filePath in sorted(os.listdir(path)):
         fileExt = os.path.splitext(filePath)[1]
@@ -20,26 +21,28 @@ def readImages(path):
 
             if im is None:
                 print("image:{} not read properly".format(imagePath))
-            else :
+            else:
                 im = cv2.resize(im, (100, 100))
-                # Convert image to floating point.
-                im = np.float32(im)/255.0
+                # Convert to grayscale
+                im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+                # Reshape the shape to total pixels (10,000 because 100x100)
+                im = im.reshape(10000)
                 # Add image to list.
                 images.append(im)
-                # Flip image.
-                imFlip = cv2.flip(im, 1);
-                # Append flipped image.
-                images.append(imFlip)
+                classNames.append(os.path.splitext(filePath)[0])
 
-    numImages = int(len(images) / 2)
+    numImages = int(len(images))
 
     # Exit if no image found.
-    if numImages == 0 :
+    if numImages == 0:
         print("No images found")
         sys.exit(0)
 
+    images = np.asarray(images)
+    images = images.transpose()
+
     print(str(numImages) + " files read.")
-    return images
+    return images, classNames
 
 
 # Create data matrix from a list of images.
@@ -61,24 +64,52 @@ def createDataMatrix(images):
 
 if __name__ == '__main__':
     PATH = 'faces'
-    NUM_EIGEN_FACES = 10
+    NUM_EIGEN_FACES = 100
 
     # Read the images
-    images = readImages(PATH)
+    images, classNames = readImages(PATH)
 
-    # Size of images
-    sz = images[0].shape
+    avg_face_vector = images.mean(axis=1)
+    avg_face_vector = avg_face_vector.reshape(images.shape[0], 1)
+    normalized_face_vector = images - avg_face_vector
 
-    # Create the data matrix, each row represents 1 image
-    data = createDataMatrix(images)
+    print("Calculating PCA...")
+    covariance_matrix = np.cov(np.transpose(normalized_face_vector))
+    print(covariance_matrix)
 
-    # Compute the eigenvectors from the stack of images created.
-    print("Calculating PCA ", end="...")
+    eigen_values, eigen_vectors = np.linalg.eig(covariance_matrix)
+    print(eigen_vectors.shape)
 
-    # Get the mean and eigenVectors from PCA calculation
-    mean, eigenVectors = cv2.PCACompute(data, mean=None, maxComponents=NUM_EIGEN_FACES)
+    k_eigen_vectors = eigen_vectors[0:NUM_EIGEN_FACES, :]
+    print(k_eigen_vectors.shape)
 
-    print("DONE")
+    eigen_faces = k_eigen_vectors.dot(np.transpose(normalized_face_vector))
+    print(eigen_faces.shape)
+
+    weights = np.transpose(normalized_face_vector).dot(np.transpose(eigen_faces))
+    print(weights)
+
+    print("Done.")
+
+    test_img = cv2.imread("test/test_bill.jpg")
+
+    print("Searching for face location...")
+    detected_test_face = face_recognition.face_locations(test_img)
+    if len(detected_test_face) == 0:
+        print("No face detected.")
+        sys.exit()
+
+    test_img = cv2.cvtColor(test_img, cv2.COLOR_RGB2GRAY)
+    test_img = cv2.resize(test_img, (100, 100))
+    test_img = test_img.reshape(10000, 1)
+    test_normalized_face_vector = test_img - avg_face_vector
+    test_weight = np.transpose(test_normalized_face_vector).dot(np.transpose(eigen_faces))
+
+    print("Determining match...")
+    print(np.linalg.norm(test_weight - weights, axis=1))
+    index = np.argmin(np.linalg.norm(test_weight - weights, axis=1))
+
+    print(f"Matching image: {classNames[index]}")
 
 
 
